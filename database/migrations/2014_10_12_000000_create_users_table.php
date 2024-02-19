a6fa4598a86a76d484178532750cccb1911c26cc
artapamudaid <pamuda.id@gmail.com>
2024-02-19 15:56:01 +0700

    create migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,19 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+
+
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
+
+            $table->text('address')->nullable();
+            $table->string('housenumber')->nullable();
+            $table->string('phonenumber')->nullable();
+            $table->string('city')->nullable();
+
+            $table->string('role')->default('USER');
+
+            $table->softDeletes();
             $table->timestamps();
         });
     }
